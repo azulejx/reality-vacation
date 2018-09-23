90b0db5a7c0bf8577f2bdaa381a4803c746cefff
Azul Rodriguez Da Silva <azulejx@gmail.com>
2018-09-23 13:35:40 -0700

    add quiz text, css styles, js scripts

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+
+$(document).ready(function() {
+  $("form#form").submit(function(event) {
+    event.preventDefault();
+
+    var q1 = parseInt($(".question1").val());
+    var q2 = parseInt($(".question2").val());
+    var q3 = parseInt($(".question3").val());
+    var q4 = parseInt($(".question4").val());
+    var q5 = parseInt($(".question5").val());
+    var results = q1 + q2 + q3 + q4 + q5;
+
+    if(results === 200 || results <= 110 && results !== 30 && results !== 40 && results !== 50){
+      $("#marquez").show();
+    }
+
+    else if(results === 30 || results === 40 || results === 50 || results === 130 || results === 140 || results === 230){
+      $("#butler").show();
+    }
+
+    else if(results >= 300){
+      $("#leguin").show();
+    }
+
+    else if(results === 120 || results === 210 || results === 220){
+      $("#tie").show();
+    }
+
+    else{
+      $("#error").show();
+    }
+  });
+
+});
