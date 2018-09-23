9d52c48f3c4f0f387b82d4b6f6940426acc6eeb0
Azul Rodriguez Da Silva <azulejx@gmail.com>
2018-09-23 14:44:03 -0700

    modify answer row, add more text and styling.

--- js/scripts.js
+++ js/scripts.js
@@ -11,14 +11,18 @@ $(document).ready(function() {
     var results = q1 + q2 + q3 + q4 + q5;
 
     if(results === 200 || results <= 110 && results !== 30 && results !== 40 && results !== 50){
+      $("#youranswer").show();
       $("#marquez").show();
+      $("#readit").show();
     }
 
     else if(results === 30 || results === 40 || results === 50 || results === 130 || results === 140 || results === 230){
+      $("#youranswer").show();
       $("#butler").show();
     }
 
     else if(results >= 300){
+      $("#youranswer").show();
       $("#leguin").show();
     }
 
